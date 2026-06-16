f7fc194c0e0957a84a71ec8ac3c2c37bdd043ffa
mariojrepisus <mariojr.episus@gmail.com>
2026-06-16 01:02:50 -0300

    Update criar_pastas.R

--- R/criar_pastas.R
+++ R/criar_pastas.R
@@ -13,30 +13,6 @@
 #' \dontrun{
 #'   criar_pastas()
 #' }
-criar_pastas <- function() {
-  pasta_geral <- paste0(here::here(), "/")
-
-  criar_se_nao_existe <- function(caminho) {
-    if (!dir.exists(caminho)) dir.create(caminho)
-    caminho
-  }
-
-  pasta_resultados <- criar_se_nao_existe(paste0(pasta_geral, "resultados/"))
-
-  criar_se_nao_existe(paste0(pasta_geral, "bancos_brutos/"))
-  criar_se_nao_existe(paste0(pasta_geral, "dados auxiliares/"))
-  criar_se_nao_existe(paste0(pasta_geral, "dados_tratados/"))
-  criar_se_nao_existe(paste0(pasta_geral, "dados_inter/"))
-  criar_se_nao_existe(paste0(pasta_resultados, "descritivo/"))
-  criar_se_nao_existe(paste0(pasta_resultados, "bivariada/"))
-  criar_se_nao_existe(paste0(pasta_resultados, "multivariada/"))
-  criar_se_nao_existe(paste0(pasta_resultados, "acuracia/"))
-
-#' Criação de uma infraestrutura de pastas mínimas, de facil referenciamento, dentro da pasta colocada como diretório de trabalho setwd()
-#'
-#' Criação de uma infraestrutura mínima de pastas, de facil referenciamento, 
-#' dentro da pasta colocada como diretório de trabalho setwd() ou da pasta básica do
-#' do projeto do R
 
 criar_pastas = function(){
   library(here)
